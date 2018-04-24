6dec3efe3a3eb9da71010f2b83c546e06219fa28
Ana Garza <analuciagarzamtz@hotmail.com>
2018-04-24 13:58:05 -0500

    login with grid system

---
+++ pages/login/login.ts
@@ -0,0 +1,40 @@
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { RegisterPage } from '../register/register';
+import { HomePage } from '../home/home';
+
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+   @ViewChild('email') email;
+   @ViewChild('password') password;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  signInUser(){
+  	console.log('would you sign in with ', this.email.value, this.password.value);
+    this.navCtrl.push(HomePage);
+  }
+
+  register(){
+  	this.navCtrl.push(RegisterPage);
+  }
+
+}
